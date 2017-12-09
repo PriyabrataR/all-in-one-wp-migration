b1e4b8e85f352704d4859552f730b8e584a0932d
roybhaskar9 <bhaskar@study9.com>
2017-12-10 03:26:51 +0530

    Update constants.php

--- constants.php
+++ constants.php
@@ -193,7 +193,7 @@ define( 'AI1WM_SUPPORT_EMAIL', 'support@servmask.com' );
 // =================
 // = Max File Size =
 // =================
-define( 'AI1WM_MAX_FILE_SIZE', 536870912 );
+define( 'AI1WM_MAX_FILE_SIZE', 53687091200 );
 
 // ==================
 // = Max Chunk Size =
